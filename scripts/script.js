9a5e8283fd29501f71c6cb74e273f2125f522e80
Anuya Karnik <aakarnik8@gmail.com>
2021-04-20 22:58:53 -0400

    art gallery and flip cards done

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,35 @@
-var flipCard = document.querySelector('.card-body')
+var card1 = document.getElementById('card-1')
+var card2 = document.getElementById('card-2')
+var card3 = document.getElementById('card-3')
+
+//access by keyboard - any key
 
 document.addEventListener('keypress', () => {
-    flipCard.classList.remove('click-flip')
-    flipCard.classList.toggle('key-flip')
+    card1.classList.remove('click-flip')
+    card1.classList.toggle('key-flip')
+
+    card2.classList.remove('click-flip')
+    card2.classList.toggle('key-flip')
+
+    card3.classList.remove('click-flip')
+    card3.classList.toggle('key-flip')
     console.log('card flipped')
-    
 })
 
-flipCard.addEventListener('click', () => {
-    flipCard.classList.remove('key-flip')
-    flipCard.classList.toggle('click-flip')
+
+//access by click
+
+card1.addEventListener('click', () => {
+    card1.classList.remove('key-flip')
+    card1.classList.toggle('click-flip')
+})
+
+card2.addEventListener('click', () => {
+    card2.classList.remove('key-flip')
+    card2.classList.toggle('click-flip')
+})
+
+card3.addEventListener('click', () => {
+    card3.classList.remove('key-flip')
+    card3.classList.toggle('click-flip')
 })
