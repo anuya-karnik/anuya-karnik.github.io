fd68e95872792ad9156840f64c25e9f0cafb2e0e
Anuya Karnik <aakarnik8@gmail.com>
2021-12-07 11:54:46 -0500

    resume update

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,12 @@ ScrollReveal().reveal('.grid-item', {interval: 50, origin:'bottom', distance: '1
 /**********
 IMAGE MODALS
 ***********/
+
+ScrollReveal().reveal('.scroll-in', { interval: 50, origin: 'bottom', distance: '10px', easing: 'ease-in' });
+
+/**********
+IMAGE MODALS
+***********/
 var allImages = document.querySelectorAll('.pr-img')
 for (var imgLayout of allImages) {
     var imgDisplay = imgLayout.querySelector('img')
@@ -62,38 +68,43 @@ for (var openImg of openImages) {
 FLIP CARDS
 todo: add try-except here
 ***********/
-var card1 = document.getElementById('card-1')
-var card2 = document.getElementById('card-2')
-var card3 = document.getElementById('card-3')
+try {
+    var card1 = document.getElementById('card-1')
+    var card2 = document.getElementById('card-2')
+    var card3 = document.getElementById('card-3')
 
-//access by keyboard - any key
+    //access by keyboard - any key
 
-document.addEventListener('keypress', () => {
-    card1.classList.remove('click-flip')
-    card1.classList.toggle('key-flip')
+    document.addEventListener('keypress', () => {
+        card1.classList.remove('click-flip')
+        card1.classList.toggle('key-flip')
 
-    card2.classList.remove('click-flip')
-    card2.classList.toggle('key-flip')
+        card2.classList.remove('click-flip')
+        card2.classList.toggle('key-flip')
 
-    card3.classList.remove('click-flip')
-    card3.classList.toggle('key-flip')
-    console.log('card flipped')
-})
+        card3.classList.remove('click-flip')
+        card3.classList.toggle('key-flip')
+        console.log('card flipped')
+    })
 
 
-//access by click
+    //access by click
 
-card1.addEventListener('click', () => {
-    card1.classList.remove('key-flip')
-    card1.classList.toggle('click-flip')
-})
+    card1.addEventListener('click', () => {
+        card1.classList.remove('key-flip')
+        card1.classList.toggle('click-flip')
+    })
 
-card2.addEventListener('click', () => {
-    card2.classList.remove('key-flip')
-    card2.classList.toggle('click-flip')
-})
+    card2.addEventListener('click', () => {
+        card2.classList.remove('key-flip')
+        card2.classList.toggle('click-flip')
+    })
 
-card3.addEventListener('click', () => {
-    card3.classList.remove('key-flip')
-    card3.classList.toggle('click-flip')
-})
+    card3.addEventListener('click', () => {
+        card3.classList.remove('key-flip')
+        card3.classList.toggle('click-flip')
+    })
+}
+catch {
+    console.log('not this page')
+}
